1be7f254934679cc340fafb99d16b26d6c431e57
nick-crouch <92570328+nick-crouch@users.noreply.github.com>
2021-12-10 16:46:01 +0100

    cambio tabella

--- Esercitazione 9/vota.x
+++ Esercitazione 9/vota.x
@@ -2,9 +2,10 @@ const N=7;
 const NUMCOL=6;
 const MAXS = 256;
 
+
 struct Input{ char[MAXS] cand ; char op ;};
-struct Riga { char[MAXS] col [NUMCOL];
-struct Tab { Riga riga[N]; };
+struct Show { char[MAXS] candidato; char[MAXS] giudice; char categoria[N]; char[MAXS] nomeFile; char fase; int voto; };
+struct Tab { Show show[N]};
 program VOTA {
     version VOTAVERS{
     Tab CLASSIFICA_GIUDICI(void) = 1;
--- Esercitazione 9/vota_s.c
+++ Esercitazione 9/vota_s.c
@@ -19,10 +19,10 @@
 const NUMCOL=6;//NUMFILE*/
 
 /*STATO SERVER*/
-static Giudici giudici; // variabile per i giudici
+static Tab tab;
 static int inizializzato = 0// inizializzazione attuata
 
-void inizializza() //Possibilità 1
+/*void inizializza() //Possibilità 1
 {
     int i, j;
     if(inizializzato== 1) return;
@@ -50,23 +50,60 @@ void inizializza() //Possibilità 1
     tab.riga[2].col[5]=50;
     inizializzato = 1;
     printf("Terminata init struttura dati !\n");
+}*/
+
+void inizializza() //Possibilità 1
+{
+    int i, j;
+    if(inizializzato== 1) return;
+    //inizializzazione struttura dati
+    for( i= 0; i< N; i++){
+       tab.show[i].candidato = "L";
+       tab.show[i].giudice = "L";
+       tab.show[i].categoria= 'L';
+       tab.show[i].nomeFile= "L";
+       tab.show[i].fase='L';
+       tab.show[i].voto= -1;
+         
+    }
+   
+    //Eventuale riempimento altri posti
+    tab.show[2].candidato="Brasco";
+    tab.show[2].giudice="Bowie";
+    tab.show[2].categoria='U';
+    tab.show[2].categoria="BrascoProfile.txt";
+    tab.show[2].nomeFile='A';
+    tab.show[2].voto=100;
+    
+    tab.show[3].candidato="Viga";
+    tab.show[3].giudice="Winehouse";
+    tab.show[3].categoria='D';
+    tab.show[3].categoria="VigaProfile.txt";
+    tab.show[3].nomeFile='S';
+    tab.show[3].voto=50;
+    
+    inizializzato = 1;
+    printf("Terminata init struttura dati !\n");
 }
 
 /*Procedure*/
 int * esprimi_voto_1_svc(Input *input struct svc_req *rqstp)
 {
+   /* int voto = 0;
+    printf("\nInserire voto: ");
+    scanf("%d", &voto);*///prodecura remota, non ha senso chiedere ulteriori dati
     static int result =-1;
     result=-1;
     inizializza();//Invoco l’inizializzazione
     
      for( i= 0; i< N; i++){
-        if(strcmp(tab.riga[i].col[0], input->cand)==0){
+        if(strcmp(tab.show[i].candidato, input->cand)==0){
             if(strcmp(input->op, "A")==0){
-                tab.riga[i].col[NUMCOL-1] += voto;//variabile voto?
+                tab.show[i].voto += 1;//variabile voto?
                 result=0 ;
             }
             if(strcmp(input->op, "S")==0){
-                tab.riga[i].col[NUMCOL-1] -= voto;
+                tab.show[i].voto -= 1;
                 result=0;
             }
         }
@@ -80,12 +117,16 @@ Tab* classifica_giudici_1_svc(void *in, struct svc_req *rqstp)
     inizializza(); //Invoco l’inizializzazione
     
     
-     for( i= 0; i< N; i++){
-    for(j= 0; j <NUMCOL; j++){
-        if(j==NUMCOL-1) tab.riga[i].col[j]=-1;
-         else tab.riga[i].col[j]='L';
-         
-    }
+     /*for( i= 0; i< N; i++){
+        tab.show[i].candidato = "L";
+       tab.show[i].giudice = "L";
+       tab.show[i].categoria= 'L';
+       tab.show[i].nomeFile= "L";
+       tab.show[i].fase='L';
+       tab.show[i].voto= -1;
+    }*/
+     
+     
     return (&tab);
 } //classifica_giudici
 
